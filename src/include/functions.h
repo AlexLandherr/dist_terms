a3bd2488559bc1e70987850912245636fbe7b5e3
Alex Landherr <alex.landherr@gmail.com>
2023-12-14 21:12:39 +0000

    Solved stated problem.

--- src/include/functions.h
+++ src/include/functions.h
@@ -6,6 +6,8 @@
 namespace func {
     std::string add_num_str(std::string &str1, std::string &str2);
     std::string multiply_num_str(std::string &str1, std::string &str2);
+    std::string power_of_str(std::string &base, int exponent);
+    int distinct_term_count(int lower_limit_a, int upper_limit_a, int lower_limit_b, int upper_limit_b);
 }
 
 #endif
